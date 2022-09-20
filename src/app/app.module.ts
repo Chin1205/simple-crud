23500b09edd7c1822a904a43b99ddb3f96b602b5
Chin1205 <gwan.chinmae07@gmail.com>
2022-09-20 17:44:50 +0800

    Exercise2 Angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,21 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { ProfileComponent } from './user/pages/profile/profile.component';
+import { FormComponent } from './user/components/form/form.component';
+import { BlogListComponent } from './blog/pages/blog-list/blog-list.component';
+import { BlogItemComponent } from './blog/components/blog-item/blog-item.component';
+import { BookListComponent } from './book/pages/book-list/book-list.component';
+import { BookItemComponent } from './book/components/book-item/book-item.component';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProfileComponent,
+    FormComponent,
+    BookListComponent,
+    BookItemComponent,
+    BlogListComponent,
+    BlogItemComponent
   ],
   imports: [
     BrowserModule,
